2b9bf5bab85ae6ad76bb4a8a54daf7acfe287d77
Angie Fiorella Alberto Guzmán <116392231+fiorexyz@users.noreply.github.com>
2026-03-03 13:29:39 -0600

    cambio en nombre de archivo
    authservice

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
-import { loginUser } from '../../services/authService';
+import { loginUser } from '../../services/authservice';
 import { useAuthStore } from '../../store/authStore';
 export default function Login() {
 const navigate = useNavigate();
